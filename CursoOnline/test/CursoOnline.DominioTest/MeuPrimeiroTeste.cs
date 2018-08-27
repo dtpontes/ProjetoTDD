4f9bd53168bb1e658ef710bef8acb05e1e305c49
Daniel <dtpontesinfo@gmail.com>
2018-08-27 11:30:34 -0300

    Primeiro Commit

---
+++ CursoOnline/test/CursoOnline.DominioTest/MeuPrimeiroTeste.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoOnline.DominioTest
+{
+    public class MeuPrimeiroTeste
+    {
+        [Fact(DisplayName = "DevemVariaveisTeremMesmoValor")]
+        public void DevemVariaveisTeremMesmoValor()
+        {
+
+            //Oraganização
+            var variavel1 = 1;
+            var variavel2 = 5;
+
+            //Ação
+            variavel2 = variavel1;
+
+
+            //Assert
+            Assert.Equal(variavel1, variavel2);
+
+
+
+
+        }
+    }
+}
